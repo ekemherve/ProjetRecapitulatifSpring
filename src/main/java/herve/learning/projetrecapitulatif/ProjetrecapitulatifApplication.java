322799d5bc3646896e05eed7c8b58d016e0b863f
Romuald Ekem <RomualdHerve.EkemKingni@busi.eu>
2019-03-23 10:32:52 +0100

    projet de base

---
+++ src/main/java/herve/learning/projetrecapitulatif/ProjetrecapitulatifApplication.java
@@ -0,0 +1,13 @@
+package herve.learning.projetrecapitulatif;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetrecapitulatifApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetrecapitulatifApplication.class, args);
+    }
+
+}
